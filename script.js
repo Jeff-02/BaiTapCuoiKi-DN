6238db69acdac48cd7a3d49839ce389d4c06a8e4
Nguyễn Đức Trường <truongnguyenduc062@gmail.com>
2026-05-22 23:29:19 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuItems = document.querySelectorAll('.menu-item');
+const mainTitle = document.getElementById('main-title');
+menuItems.forEach(item => {
+    item.addEventListener('click', function() {        
+        menuItems.forEach(el => el.classList.remove('active'));       
+        this.classList.add('active');       
+        const newTitle = this.getAttribute('data-title');
+        mainTitle.textContent = newTitle;
+    });
+});
